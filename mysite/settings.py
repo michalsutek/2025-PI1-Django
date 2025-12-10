86fc597b29104e328b89849e0b9789998736df9d
Michal Šutek <sutek.spsit@gmail.com>
2025-12-10 09:25:10 +0100

    add app skuska2

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'skuska',
     'kalkulacka',
+    'skuska2',
 ]
 
 MIDDLEWARE = [
---
+++ skuska2/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Osoba, Student
+
+admin.site.register(Osoba)
+admin.site.register(Student)
